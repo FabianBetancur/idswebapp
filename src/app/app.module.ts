300e5065cc3e153097184aabd9deeacab7ca96a3
edfabianb <edfabianb@gmail.com>
2024-03-24 17:35:41 -0500

    change icon tab browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
-
 import { TokenInterceptor } from '@interceptors/token.interceptor';
+import { environment } from '@environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
